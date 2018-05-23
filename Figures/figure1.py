e2264d7d841b0103c467e4c93b58c8c4c1686702
Laura Kreidberg <lkreidberg@nighthawk.cfa.harvard.edu>
2018-05-23 11:58:30 -0400

    edits

--- Figures/figure1.py
+++ Figures/figure1.py
@@ -16,6 +16,8 @@ sns.set_style("ticks", {"xtick.direction":"in", "ytick.direction":"in"})
 #equivalent widths from Antonija's email
 width_a = 0.056/70.*1e6
 width_b = 0.003/70.*1e6
+width_c = 0.02/70.*1e6
+width_d = 0.000/70.*1e6
 
 #wave_bins = np.array([8360, 8998, 9598, 10198, 10798, 10868, 11290])/1e4
 d= np.genfromtxt("w12_g102_tspec_rp_tied_mcmc_with_dilution.txt")
@@ -31,7 +33,7 @@ diff_sig = np.sqrt(sig**2 + d[1,2]**2)*1e6
 observed = (d[1,1] - mu)*1e6
 
 #calculate significance of difference with He line
-print "observed feature (ppm)", observed
+print "observed feature (ppm)", observed, "+/-", diff_sig
 print "detection significance, ppm error (model A)", (width_a - observed)/diff_sig, diff_sig
 print "detection significance, ppm error (model B)", (width_b - observed)/diff_sig, diff_sig
 
@@ -40,7 +42,8 @@ print "mean flux decrement", 1 - np.mean(d[:,1])
 
 plt.figure(figsize = (6.5,3))
 plt.errorbar(d[:,0], d[:,1]*1e6 - np.mean(d[:,1]*1e6), yerr = d[:,2]*1e6, xerr
-=xerr, linestyle = 'none', zorder = 100, marker = 'o', color = 'w', markeredgecolor = 'k', ecolor ='k', markeredgewidth = 1.3)
+=xerr, linestyle = 'none', zorder = 100, marker = 'o', color = 'w',
+markeredgecolor = 'k', ecolor ='k', markeredgewidth = 1.2, linewidth = 1.0)
 
 plt.axhline(0, linestyle = 'dotted', color = '0.5', zorder=-100)
 
@@ -50,10 +53,16 @@ He = 10830.
 
 x = np.array([He - delta, He - delta, He + delta, He + delta])/1e4
 y = np.array([0., width_a, width_a, 0.])
-plt.plot(x, y, color = '#0165fc', label = 'Model A (all gas)')
+plt.plot(x, y, color = '#0165fc', label = 'Model A (all gas)', zorder= -10)
 
 y = np.array([0., width_b, width_b, 0.])
-plt.plot(x, y, color = '#fd411e', label = 'Model B (Roche only)')
+plt.plot(x, y, color = '#fd411e', label = 'Model A (Roche only)', zorder = -8)
+
+y = np.array([0., width_c, width_c, 0.])
+plt.plot(x, y, color = 'orange', label = 'Model B (all gas)', zorder = -9)
+
+y = np.array([0., width_d, width_d, 0.])
+plt.plot(x, y, color = 'cyan', label = 'Model B (Roche only)', zorder = -7)
 
 plt.legend(loc = 'upper left')
 plt.xlabel("Wavelength (microns)")
